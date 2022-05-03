075edf7bf75282cc9e2d70e91a29c9d59b034f9e
kvcsmiki <kovacs.miklos.szabolcs@gmail.com>
2022-05-03 11:35:38 +0200

    drawing the table

--- src/main/java/labyrinth/Model.java
+++ src/main/java/labyrinth/Model.java
@@ -6,28 +6,32 @@ public class Model {
 
     private int steps;
     private Position ballPos;
-    private Position victoryPos;
+    private final Position victoryPos;
     private Direction direction;
     private boolean victory;
     private ArrayList<Position> allPos;
 
     public Model(){
         steps = 0;
-        ballPos = new Position(5,2);
-        victoryPos = new Position(3,6);
+        ballPos = new Position(4,1);
+        victoryPos = new Position(2,5);
         direction = Direction.NONE;
         victory = false;
         allPos = new ArrayList<>(); fill();
         buildWalls();
-
+        printAllWalls();
     }
 
     private void fill(){
-        for(int y=1;y<8;y++){
-            for(int x=1;x<8;x++){
+        for(int y=0;y<7;y++){
+            for(int x=0;x<7;x++){
                 allPos.add(new Position(x,y));
             }
         }
+    }
+
+    public ArrayList<Position> getAllPos() {
+        return allPos;
     }
 
     private Position getFromAllPos(Position pos){
@@ -43,25 +47,25 @@ public class Model {
         pos.getWalls().add(wall);
         switch (wall){
             case UP -> {
-                if(pos.getY() == 1)
+                if(pos.getY() == 0)
                     return;
                 Position temp = new Position(pos.getX(),pos.getY()-1);
                 buildWall(getFromAllPos(temp),Direction.DOWN);
             }
             case DOWN -> {
-                if(pos.getY() == 7)
+                if(pos.getY() == 6)
                     return;
                 Position temp = new Position(pos.getX(),pos.getY()+1);
                 buildWall(getFromAllPos(temp),Direction.UP);
             }
             case RIGHT -> {
-                if(pos.getX() == 7)
+                if(pos.getX() == 6)
                     return;
                 Position temp = new Position(pos.getX()+1,pos.getY());
                 buildWall(getFromAllPos(temp),Direction.LEFT);
             }
             case LEFT -> {
-                if(pos.getX() == 1)
+                if(pos.getX() == 0)
                     return;
                 Position temp = new Position(pos.getX()-1,pos.getY());
                 buildWall(getFromAllPos(temp),Direction.RIGHT);
@@ -70,39 +74,75 @@ public class Model {
     }
     private void buildWalls(){
         for(int i=0;i< allPos.size();i++){
-            if(allPos.get(i).getX() == 1)
+            if(allPos.get(i).getX() == 0)
                 buildWall(allPos.get(i),Direction.LEFT);
-            if(allPos.get(i).getX() == 7)
+            if(allPos.get(i).getX() == 6)
                 buildWall(allPos.get(i),Direction.RIGHT);
-            if(allPos.get(i).getY() == 1)
+            if(allPos.get(i).getY() == 0)
                 buildWall(allPos.get(i),Direction.UP);
-            if(allPos.get(i).getY() == 7)
+            if(allPos.get(i).getY() == 6)
                 buildWall(allPos.get(i),Direction.DOWN);
         }
-        buildWall(getFromAllPos(1,1),Direction.RIGHT);
-        buildWall(getFromAllPos(3,1),Direction.DOWN);
-        buildWall(getFromAllPos(4,1),Direction.RIGHT);
-        buildWall(getFromAllPos(7,1),Direction.DOWN);
-        buildWall(getFromAllPos(2,3),Direction.DOWN);
+        buildWall(getFromAllPos(0,0),Direction.RIGHT);
+        buildWall(getFromAllPos(2,0),Direction.DOWN);
+        buildWall(getFromAllPos(3,0),Direction.RIGHT);
+        buildWall(getFromAllPos(6,0),Direction.DOWN);
+        buildWall(getFromAllPos(1,2),Direction.DOWN);
+        buildWall(getFromAllPos(2,2),Direction.RIGHT);
+        buildWall(getFromAllPos(5,2),Direction.RIGHT);
+        buildWall(getFromAllPos(3,3),Direction.DOWN);
         buildWall(getFromAllPos(3,3),Direction.RIGHT);
-        buildWall(getFromAllPos(6,3),Direction.RIGHT);
+        buildWall(getFromAllPos(4,3),Direction.RIGHT);
+        buildWall(getFromAllPos(6,3),Direction.DOWN);
+        buildWall(getFromAllPos(0,4),Direction.DOWN);
         buildWall(getFromAllPos(4,4),Direction.DOWN);
-        buildWall(getFromAllPos(4,4),Direction.RIGHT);
-        buildWall(getFromAllPos(5,4),Direction.RIGHT);
-        buildWall(getFromAllPos(7,4),Direction.DOWN);
-        buildWall(getFromAllPos(1,5),Direction.DOWN);
-        buildWall(getFromAllPos(5,5),Direction.DOWN);
-        buildWall(getFromAllPos(3,6),Direction.LEFT);
-        buildWall(getFromAllPos(3,6),Direction.DOWN);
+        buildWall(getFromAllPos(2,5),Direction.LEFT);
+        buildWall(getFromAllPos(2,5),Direction.DOWN);
+        buildWall(getFromAllPos(2,5),Direction.RIGHT);
         buildWall(getFromAllPos(3,6),Direction.RIGHT);
-        buildWall(getFromAllPos(4,7),Direction.RIGHT);
-        buildWall(getFromAllPos(6,7),Direction.RIGHT);
+        buildWall(getFromAllPos(5,6),Direction.RIGHT);
     }
     private void printAllWalls(){
         for(int i=0;i<allPos.size();i++)
             System.out.println("pos: "+allPos.get(i)+" walls: "+allPos.get(i).getWalls());
     }
 
+    public void moveBall(Direction direction){
+        if(ballPos.equals(victoryPos)){
+            victory = true;
+            return;
+        }
+        Position pos = getFromAllPos(ballPos);
+        if(pos.getWalls().contains(direction))
+            return;
+        switch (direction){
+            case UP -> {
+                ballPos.setY(ballPos.getY()-1);
+                moveBall(Direction.UP);
+            }
+            case DOWN -> {
+                ballPos.setY(ballPos.getY()+1);
+                moveBall(Direction.UP);
+            }
+            case LEFT -> {
+                ballPos.setX(ballPos.getX()-1);
+                moveBall(Direction.UP);
+            }
+            case RIGHT -> {
+                ballPos.setX(ballPos.getX()+1);
+                moveBall(Direction.UP);
+            }
+        }
+    }
+    public boolean isVictory(){
+        return victory;
+    }
 
 
+    public Position getBallPos() {
+        return ballPos;
+    }
+    public Position getVictoryPos(){
+        return victoryPos;
+    }
 }
--- src/main/java/labyrinth/Scene2Controller.java
+++ src/main/java/labyrinth/Scene2Controller.java
@@ -2,7 +2,12 @@ package labyrinth;
 
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+import java.util.ArrayList;
 
 public class Scene2Controller {
     @FXML
@@ -15,12 +20,84 @@ public class Scene2Controller {
 
     Model model;
 
+    private final int cellSize = 100;
+
     public void initialize(){
         model = new Model();
+        drawStart();
     }
 
+    private GraphicsContext getGraphicsContext(){
+        return canvas.getGraphicsContext2D();
+    }
 
     public void setName(String name){
         this.name = name;
     }
+
+    private void drawStart(){
+        drawCells();
+        drawWalls();
+        drawBall();
+        drawFinish();
+    }
+    private void drawCells(){
+        GraphicsContext gc = getGraphicsContext();
+        ArrayList<Position> allPos = model.getAllPos();
+        for(int i=0;i<allPos.size();i++){
+            gc.setFill(Color.BLACK);
+            gc.fillRect(allPos.get(i).getX()*cellSize,allPos.get(i).getY()*cellSize,cellSize,cellSize);
+            gc.setFill(Color.WHITE);
+            gc.fillRect(allPos.get(i).getX()*cellSize+1,allPos.get(i).getY()*cellSize+1,cellSize-1,cellSize-1);
+        }
+    }
+    private void drawWalls(){
+        GraphicsContext gc = getGraphicsContext();
+        gc.setLineWidth(4);
+        ArrayList<Position> allPos = model.getAllPos();
+        for(int i=0;i<allPos.size();i++){
+            for(int j=0;j<allPos.get(i).getWalls().size();j++){
+                switch(allPos.get(i).getWalls().get(j)){
+                    case UP -> {
+                        gc.moveTo(allPos.get(i).getX()*cellSize,allPos.get(i).getY()*cellSize);
+                        gc.lineTo(allPos.get(i).getX()*cellSize+cellSize,allPos.get(i).getY()*cellSize);
+                        gc.stroke();
+                    }
+                    case DOWN -> {
+                        gc.moveTo(allPos.get(i).getX()*cellSize,allPos.get(i).getY()*cellSize+cellSize);
+                        gc.lineTo(allPos.get(i).getX()*cellSize+cellSize,allPos.get(i).getY()*cellSize+cellSize);
+                        gc.stroke();
+                    }
+                    case LEFT -> {
+                        gc.moveTo(allPos.get(i).getX()*cellSize,allPos.get(i).getY()*cellSize);
+                        gc.lineTo(allPos.get(i).getX()*cellSize,allPos.get(i).getY()*cellSize+cellSize);
+                        gc.stroke();
+                    }
+                    case RIGHT -> {
+                        gc.moveTo(allPos.get(i).getX()*cellSize+cellSize,allPos.get(i).getY()*cellSize);
+                        gc.lineTo(allPos.get(i).getX()*cellSize+cellSize,allPos.get(i).getY()*cellSize+cellSize);
+                        gc.stroke();
+                    }
+                }
+            }
+        }
+    }
+
+    private void drawBall(){
+        GraphicsContext gc = getGraphicsContext();
+        Position ballPos = model.getBallPos();
+        gc.setFill(Color.BLUE);
+        gc.fillOval(ballPos.getX()*cellSize+cellSize/4, ballPos.getY()*cellSize+cellSize/4, cellSize/2,cellSize/2);
+    }
+
+    private void drawFinish(){
+        Position victoryPos = model.getVictoryPos();
+        GraphicsContext gc = getGraphicsContext();
+        gc.setFill(Color.BLACK);
+        gc.setFont(new Font("Calibri",38));
+        gc.fillText("CÉL", victoryPos.getX()*cellSize + 23, victoryPos.getY()*cellSize + cellSize/2*1.2);
+    }
+
+
+
 }
